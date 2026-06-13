bc1961d08e923a665deb4579d4fb943b909440ca
aisyah307 <aisyahd307@gmail.com>
2026-06-13 18:32:09 +0700

    update calculator

--- calculator.py
+++ calculator.py
@@ -19,3 +19,14 @@ def power(a: float, b: float) -> float:
 
 def modulus(a: float, b: float) -> float:
     return a % b
+
+def check_number_status(x: float) -> str:
+    if x > 100:
+        if x % 2 == 0:
+            return "Large Even"
+        else:
+            return "Large Odd"
+    elif x < 0:
+        return "Negative"
+    else:
+        return "Normal"
