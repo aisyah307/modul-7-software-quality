48a9da31ff0365802eb56974f75157004d2ff710
aisyah307 <aisyahd307@gmail.com>
2026-06-16 08:21:07 +0700

    update calculator

--- calculator.py
+++ calculator.py
@@ -10,23 +10,3 @@ def divide(a: float, b: float) -> float:
 
 def subtract(a: float, b: float) -> float:
     return a - b
-
-def multiply(a: float, b: float) -> float:
-    return a * b
-
-def power(a: float, b: float) -> float:
-    return a ** b
-
-def dummy_function():
-    return "Fungsi ini sengaja tidak dites"
-
-def check_number_status(x: float) -> str:
-    if x > 100:
-        if x % 2 == 0:
-            return "Large Even"
-        else:
-            return "Large Odd"
-    elif x < 0:
-        return "Negative"
-    else:
-        return "Normal"
